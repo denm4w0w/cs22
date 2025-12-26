820ddb3760bc064e5155d2a9473b13c0b68ee02b
denm4w0w <denm4w0w@gmail.com>
2025-12-26 23:08:04 +0200

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: 'all'
+  },
+  preview: {
+    host: true,
+    allowedHosts: 'all'
+  },
+  build: {
+    outDir: 'dist'
+  }
+})
